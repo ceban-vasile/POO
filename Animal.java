e465f11dff6f4978b451807157e607b4595cd24a
Ceban Vasile <vasile.ceban1@isa.utm.md>
2023-09-12 13:45:06 +0300

    Class animal with a new object.

--- Animal.java
+++ Animal.java
@@ -1,13 +1,15 @@
 public class Animal {
-    String denumire, tip;
+    String denumire, tip, sex;
+    int age;
 
-    public Animal(String denumire, String tip){
+    public Animal(String denumire, String tip, String sex, int age){
         this.denumire = denumire;
         this.tip = tip;
+        this.sex = sex;
+        this.age = age;
     }
-
     public static void main(String[] args){
-        Animal animal = new Animal("Caine", "mamifer");
-        System.out.println(animal.denumire+" "+animal.tip);
+        Animal animal = new Animal("Caine", "mamifer", "f", 2);
+        System.out.println(animal.denumire+" "+animal.tip+ " "+animal.sex+" "+animal.age);
     }
 }
