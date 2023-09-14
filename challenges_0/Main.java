a33ee43836c9ef2ccc95584bd0b5d165c77cb8fe
Ceban Vasile <vasile.ceban1@isa.utm.md>
2023-09-14 14:19:18 +0300

    I changed a Main class in challenges_0 package and I finish to solve the second problem.

--- challenges_0/Main.java
+++ challenges_0/Main.java
@@ -1,10 +1,11 @@
 package challenges_0;
-public class Main {
+public class Main extends MathsUtils {
     public static void main(String[] args){
-        User user = new User("Ion","Doe");
-        System.out.println(user.firstName+" "+user.lastName+"->"+user.firstName.charAt(0)+"."+user.lastName.charAt(0)+".");
-        user.firstName = "Miley";
-        user.lastName = "Cyrus";
-        System.out.println(user.firstName+" "+user.lastName+"->"+user.firstName.charAt(0)+"."+user.lastName.charAt(0)+".");
+//        User user = new User("Ion","Doe");
+//        System.out.println(user.firstName+" "+user.lastName+"->"+user.firstName.charAt(0)+"."+user.lastName.charAt(0)+".");
+//        user.firstName = "Miley";
+//        user.lastName = "Cyrus";
+//        System.out.println(user.firstName+" "+user.lastName+"->"+user.firstName.charAt(0)+"."+user.lastName.charAt(0)+".");
+          mathsutils(10);
     }
 }
