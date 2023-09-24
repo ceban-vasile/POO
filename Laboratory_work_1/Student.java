930d179908dd7f6bd715c3de47c36e801a435413
Ceban Vasile <vasile.ceban1@isa.utm.md>
2023-09-24 21:37:14 +0300

    I added the constructor Student.

--- Laboratory_work_1/Student.java
+++ Laboratory_work_1/Student.java
@@ -5,4 +5,12 @@ import java.util.Date;
 public class Student {
     String firstName, lastName, email;
     Date enrollmentDate, dateOfBirth;
+
+    public Student(String firstName, String lastName, String email, Date enrollmentDate, Date dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.enrollmentDate = enrollmentDate;
+        this.dateOfBirth = dateOfBirth;
+    }
 }
