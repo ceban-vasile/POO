e74cc6681843a9a36d3b330e77cb6f73995cdc08
Ceban Vasile <vasile.ceban1@isa.utm.md>
2023-09-25 19:02:21 +0300

    I changed the type of variables in static.

--- Laboratory_work_1/Student.java
+++ Laboratory_work_1/Student.java
@@ -1,16 +1,20 @@
 package Laboratory_work_1;
 
+import java.text.ParseException;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Student {
-    String firstName, lastName, email;
-    Date enrollmentDate, dateOfBirth;
+     static String firstName;
+    static String lastName;
+    static String email;
+    static Date enrollmentDate;
+    static Date dateOfBirth;
 
-    public Student(String firstName, String lastName, String email, Date enrollmentDate, Date dateOfBirth) {
+    public Student(String firstName, String lastName, String email, Date dateOfBirth) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.enrollmentDate = enrollmentDate;
         this.dateOfBirth = dateOfBirth;
     }
 }
