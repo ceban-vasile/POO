7fc4a14155247ceec7b487194f06fd66429a1404
Ceban Vasile <vasile.ceban1@isa.utm.md>
2023-09-27 23:12:32 +0300

    I called the readStudent method.

--- Laboratory_work_1/Main.java
+++ Laboratory_work_1/Main.java
@@ -1,39 +1,13 @@
 package Laboratory_work_1;
 
 import java.text.ParseException;
-import java.util.Scanner;
 
-import static Laboratory_work_1.Faculty.*;
-import static java.lang.System.exit;
+import static Laboratory_work_1.FileManager.readStudent;
+import static Laboratory_work_1.Menu.tumMenu;
 
 public class Main {
-    public static void tumMenu() throws ParseException {
-        Scanner myScanner = new Scanner(System.in);
-        System.out.println("""
-                Welcome to TUM's student management system!
-                What do you want to do?
-                g - General operations
-                f - Faculty operations
-                s - Student operations
-                
-                q - Quit program
-                """);
-        System.out.print("your input>");
-        String choice = myScanner.nextLine();
-        if(choice.equals("g")){// generalOperations();
-             }
-        else if(choice.equals("f")){
-            facultyOperations();
-        }
-        else if(choice.equals("q")){
-                System.exit(0);
-        }
-        else{
-            System.out.println("You did not enter the correct data!!!");
-            tumMenu();
-        }
-    }
     public static void main(String[] args) throws ParseException {
         tumMenu();
+        readStudent();
     }
 }
