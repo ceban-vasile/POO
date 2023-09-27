afed93c2903364f7a90c4ed034a6a744727b6cdd
Ceban Vasile <vasile.ceban1@isa.utm.md>
2023-09-27 23:09:44 +0300

    I changed the functionality of loop.

---
+++ Laboratory_work_1/Menu.java
@@ -0,0 +1,38 @@
+package Laboratory_work_1;
+
+import java.text.ParseException;
+import java.util.Scanner;
+
+import static Laboratory_work_1.OperationFaculty.menuFacultyOperations;
+import static Laboratory_work_1.OperationStudent.menuStudentFaculty;
+
+public class Menu {
+    // different class application loop ex
+    public static void tumMenu() throws ParseException {
+        Scanner inputScanner = new Scanner(System.in);
+        // close scanner
+        System.out.println("""
+                Welcome to TUM's student management system!
+                What do you want to do?
+                g - General operations
+                f - Faculty operations
+                s - Student operations
+                
+                q - Quit program
+                """);
+        String choice = "";
+
+        // give meaningful condition
+        while(!"q".equals(choice)){
+            System.out.print("your input>");
+            choice = inputScanner.nextLine();
+            switch (choice) {
+                case "g" -> menuFacultyOperations();
+                case "f" -> menuStudentFaculty();
+                case "q" -> {}
+                default ->  System.out.println("You did not enter the correct data!!!");
+            }
+            inputScanner.close();
+        }
+    }
+}
