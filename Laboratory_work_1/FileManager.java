187715087d766f0aaa1a7e13cc33fe6810c3c3d2
Ceban Vasile <vasile.ceban1@isa.utm.md>
2023-09-28 12:08:09 +0300

    I created a writerStudent method to save a new student in enrollmentStudents.txt.

--- Laboratory_work_1/FileManager.java
+++ Laboratory_work_1/FileManager.java
@@ -1,23 +1,25 @@
 package Laboratory_work_1;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static Laboratory_work_1.Student.enrollstud;
+import static Laboratory_work_1.Student.enrollstudent;
 
 public class FileManager {
+    public static void writerStudent(Student student){
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("C:\\Users\\Vasile\\IdeaProjects\\POO\\Laboratory_work_1\\enrollmentStudents.txt", true))) {
+            bufferedWriter.write(String.valueOf(student));
+        } catch (IOException e) {}
+    }
 
     public static void readStudent(){
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader("C:\\Users\\Vasile\\IdeaProjects\\POO\\Laboratory_work_1\\enrollmentStudents.txt"))) {
             String line = bufferedReader.readLine();
             while (line != null) {
                 Student student = getStudent(line);
-                enrollstud.add(student);
+                enrollstudent.add(student);
                 line = bufferedReader.readLine();
             }
         } catch (FileNotFoundException e) {
@@ -30,21 +32,20 @@ public class FileManager {
     }
 
     private static Student getStudent(String line) throws ParseException {
-        String[] parts = line.split(",");
+        String[] parts = line.split(" ");
 
         String abbreviation = parts[0];
         String firstName = parts[1];
         String lastName = parts[2];
         String email = parts[3];
         String enrol = parts[4];
-        String enrollDate = parts[5];
-        String birthdayString = parts[6];
+        String birthdayString = parts[5];
+        String enrollDate = parts[6];
 
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         Date dateEnroll = dateFormat.parse(enrollDate);
         Date dateOfBirth = dateFormat.parse(birthdayString);
 
-        Student student = new Student(abbreviation, firstName, lastName, email, enrol, dateEnroll, dateOfBirth);
-        return student;
+        return new Student(abbreviation, firstName, lastName, email, enrol, dateOfBirth, dateEnroll);
     }
 }
