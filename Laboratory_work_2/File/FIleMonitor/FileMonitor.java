f0615ff2359a4ec958c89326a8a306d3fce051c6
Vasile <vasile.ceban1@isa.utm.md>
2023-10-25 23:17:22 +0300

    I extend File class and I create writeCommand and findExtension method. I partially made the functionality of the info and writeCommand methods

--- Laboratory_work_2/File/FIleMonitor/FileMonitor.java
+++ Laboratory_work_2/File/FIleMonitor/FileMonitor.java
@@ -1,14 +1,72 @@
 package Laboratory_work_2.File.FIleMonitor;
 
-public class FileMonitor {
+import Laboratory_work_2.File.File;
+import Laboratory_work_2.File.TxtFile.TextFile;
 
-    public void commit(String directoryPath) {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileMonitor extends File{
+    TextFile txtFile;
+
+    {
+        txtFile = new TextFile();
+    }
+
+    public void commit() {
 
     }
     public void info(String fileName) {
+        String repository = "C:\\Users\\Vasile\\Desktop\\test_repository\\";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(repository+fileName));
+            String line;
 
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split("\\s+");
+                txtFile.wordCount += words.length;
+                txtFile.charCount += line.length();
+                txtFile.lineCount++;
+            }
+            txtFile.extension = findExtension(fileName);
+            txtFile.fileName = fileName;
+            txtFile.printInfo();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     public void status() {
 
     }
+
+    public void printInfo(){
+
+    }
+    private String findExtension(String fileName){
+
+        return fileName.substring(fileName.lastIndexOf('.') + 1);
+    }
+
+    public void writeCommand(){
+        Scanner inputScanner = new Scanner(System.in);
+        int isCommand = 1;
+        while(isCommand == 1){
+            System.out.print(">");
+            String command = inputScanner.next();
+            switch(command){
+                case "commit" -> {commit();}
+                case "info" -> {
+                    String fileName = inputScanner.next();
+                    info(fileName);
+                }
+                case "status" -> {status();}
+                default -> {System.out.println("Is not valid command!!!");}
+            }
+        }
+        inputScanner.close();
+    }
+
 }
--- Laboratory_work_2/File/File.java
+++ Laboratory_work_2/File/File.java
@@ -3,7 +3,7 @@ package Laboratory_work_2.File;
 import java.time.LocalDateTime;
 
 public abstract class File {
-    protected String fileName;
-    protected String extension;
+    public String fileName;
+    public String extension;
     public abstract void printInfo();
 }
