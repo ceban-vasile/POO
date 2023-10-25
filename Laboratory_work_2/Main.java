b978087ee5d26456a9609ec3bb04590334507235
Vasile <vasile.ceban1@isa.utm.md>
2023-10-25 23:08:49 +0300

    I instantiated a FIleMonitor type object with which I call the writeCommand method.

--- Laboratory_work_2/Main.java
+++ Laboratory_work_2/Main.java
@@ -1,7 +1,10 @@
 package Laboratory_work_2;
 
+import Laboratory_work_2.File.FIleMonitor.FileMonitor;
+
 public class Main{
     public static void main(String[] args) {
-
+        FileMonitor command = new FileMonitor();
+        command.writeCommand();
     }
 }
