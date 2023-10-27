04ff65953fcec2e803986c9801baeb390a5ddd06
Vasile <vasile.ceban1@isa.utm.md>
2023-10-27 10:40:01 +0300

    I remove the info method and I changed the structure menu.

--- Laboratory_work_2/FileClass/FIleMonitor/FileMonitor.java
+++ Laboratory_work_2/FileClass/FIleMonitor/FileMonitor.java
@@ -6,14 +6,9 @@ import Laboratory_work_2.FileClass.ProgramFile.ProgramFile;
 import Laboratory_work_2.FileClass.TxtFile.TextFile;
 import Laboratory_work_2.ReadFiles.OperationFile;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.nio.file.attribute.FileTime;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class FileMonitor extends Files {
@@ -29,78 +24,34 @@ public class FileMonitor extends Files {
         FileTime currentFileTime = FileTime.from(Instant.now());
         file.writeSnapshot(currentFileTime);
     }
-    public void info(String fileName) {
-        String repository = "C:\\Users\\Vasile\\Desktop\\test_repository\\";
-        List<Integer> countInfo = new ArrayList<>();
-        String extension = findExtension(fileName);
-        switch (extension) {
-            case "txt" -> {
-                countInfo.clear();
-                countInfo = file.readTxtFile(repository + fileName);
-                txtFile.wordCount = countInfo.get(0);
-                txtFile.charCount = countInfo.get(1);
-                txtFile.lineCount = countInfo.get(2);
-                txtFile.fileName = fileName;
-                txtFile.extension = extension;
-                txtFile.findCreateTime(repository + fileName);
-                txtFile.printInfo();
-            }
-            case "java" -> {
-                countInfo.clear();
-                countInfo = file.readJavaFile(repository + fileName);
-                programFile.lineCount = countInfo.get(0);
-                programFile.classCount = countInfo.get(1);
-                programFile.methodCount = countInfo.get(2);
-                programFile.fileName = fileName;
-                programFile.extension = extension;
-                programFile.findCreateTime(repository + fileName);
-                programFile.printInfo();
-            }
-            case "py" -> {
-                countInfo.clear();
-                countInfo = file.readPythonFile(repository + fileName);
-                programFile.lineCount = countInfo.get(0);
-                programFile.classCount = countInfo.get(1);
-                programFile.methodCount = countInfo.get(2);
-                programFile.fileName = fileName;
-                programFile.extension = extension;
-                programFile.findCreateTime(repository + fileName);
-                programFile.printInfo();
-            }
-            case "png", "jpg" -> {
-                countInfo.clear();
-                countInfo = file.readImageFile(repository + fileName);
-                imageFile.fileName = fileName;
-                imageFile.extension = extension;
-                imageFile.width = countInfo.get(0);
-                imageFile.height = countInfo.get(1);
-                imageFile.findCreateTime(repository + fileName);
-                imageFile.printInfo();
-            }
-        }
-    }
     public void status() {
         String repository = "C:\\Users\\Vasile\\Desktop\\test_repository\\";
         File folder = new File(repository);
         File[] files = folder.listFiles();
         snapshot = file.readSnapshot();
-        printInfo();
+        infoStatus();
         for(File file: files){
             String fileName = file.getName();
             FileTime lastModifiedTime = FileTime.fromMillis(file.lastModified());
-            int comparisonResult = lastModifiedTime.compareTo(snapshot);
-            if (comparisonResult > 0) {
+            int comparisonResultMod = lastModifiedTime.compareTo(snapshot);
+            if (comparisonResultMod > 0) {
                 System.out.println(fileName+" - Changed");
-            }else if(comparisonResult < 0){
+            }else if(comparisonResultMod < 0){
                 System.out.println(fileName+" - No changed");
             }
-
-
         }
     }
-    @Override
-    public void printInfo(){
-        System.out.println("Created Snapshot at: " + snapshot);
+    public void infoStatus(){
+
+    }
+    public void printInfo(String filename){
+       // System.out.println("Created Snapshot at: " + snapshot);
+        extension = findExtension(filename);
+        switch (extension){
+            case "txt" -> {txtFile.printInfo(filename);}
+            case "java", "py" -> {programFile.printInfo(filename);}
+            case "png", "jpg" -> {imageFile.printInfo(filename);}
+        }
     }
     private void help(){
         System.out.println("1. commit -  Simply update the snapshot time to the current time.\n" +
@@ -113,7 +64,6 @@ public class FileMonitor extends Files {
     public void findCreateTime(String filePath) {
 
     }
-
     private String findExtension(String fileName){
 
         return fileName.substring(fileName.lastIndexOf('.') + 1);
@@ -129,10 +79,11 @@ public class FileMonitor extends Files {
                 case "commit" -> {commit();}
                 case "info" -> {
                     String fileName = inputScanner.next();
-                    info(fileName);
+                    printInfo(fileName);
                 }
                 case "status" -> {status();}
-                case "help" -> {printInfo();}
+                case "help" -> {help();}
+                case "exit" -> {isCommand = 0;}
                 default -> {System.out.println("Is not valid command!!! \n" +
                         "For more information, write help in the console");}
             }
