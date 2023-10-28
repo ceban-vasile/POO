ddcc68e968590b23da365294fb273638a3a26dad
Vasile <vasile.ceban1@isa.utm.md>
2023-10-29 02:22:03 +0300

    I created the CheckChanges class to check changes in real time using a thread.

--- Laboratory_work_2/Main.java
+++ Laboratory_work_2/Main.java
@@ -1,9 +1,12 @@
 package Laboratory_work_2;
 
 import Laboratory_work_2.FileClass.Monitor.FileMonitor;
+import Laboratory_work_2.FileClass.Thread.CheckChanges;
 
 public class Main {
     public static void main(String[] args) {
+        Thread thread = new Thread(new CheckChanges());
+        thread.start();
         FileMonitor command = new FileMonitor();
         command.writeCommand();
     }
